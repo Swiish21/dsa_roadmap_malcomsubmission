ae8327be183c40c10a27b0af40d739ab156f835b
Malcom Otieno Adrian <malcolmotieno16@gmail.com>
2025-01-08 21:55:27 +0300

    added comments

--- solutions/python/Task 2 reverse an array.py
+++ solutions/python/Task 2 reverse an array.py
@@ -1,5 +1,6 @@
-def rev_arr(arr):
-    return arr[::-1]
+#below is the code to reverse an array in python
+def rev_arr(arr): #define a function and assign a parameter(arr)
+    return arr[::-1] #return the reversed array
 
-arr = [3, 6, 2, 8, 4, 10, 9, 15, 12, 7]
-print("The reversed array is: ", rev_arr(arr))
+arr = [3, 6, 2, 8, 4, 10, 9, 15, 12, 7] #sample array to be reversed
+print("The reversed array is: ", rev_arr(arr)) #print the reversed array
