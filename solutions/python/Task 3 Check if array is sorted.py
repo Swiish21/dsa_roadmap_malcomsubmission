9a77b49f53463c890cc27188472c4c98cf48fdff
Malcom Otieno Adrian <malcolmotieno16@gmail.com>
2025-01-09 13:40:15 +0300

    comments

--- solutions/python/Task 3 Check if array is sorted.py
+++ solutions/python/Task 3 Check if array is sorted.py
@@ -1,8 +1,8 @@
 def checkif_arraysorted(arr):
     if arr == sorted(arr):
-        return True
+        return True # if the array is sorted
     else:
-        return False
+        return False # if the array is not sorted
     
-arr = [3, 6, 2, 8, 4, 10, 9, 15, 12, 7]
-print(f"{arr} is sorted: {checkif_arraysorted(arr)}")
+arr = [3, 6, 2, 8, 4, 10, 9, 15, 12, 7] # array to check if it is sorted
+print(f"{arr} is sorted: {checkif_arraysorted(arr)}") # print the result
